a6e645f234cf370ab3d64b9b409cd156e74d0560
Anderson Fae <andersonfae.owl@gmail.com>
2024-10-27 22:36:40 -0300

    env.example

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -1,14 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = [
-    'strapi::logger',
-    'strapi::errors',
-    'strapi::security',
-    'strapi::cors',
-    'strapi::poweredBy',
-    'strapi::query',
-    'strapi::body',
-    'strapi::session',
-    'strapi::favicon',
-    'strapi::public',
+    "strapi::logger",
+    "strapi::errors",
+    "strapi::security",
+    {
+        name: "strapi::cors",
+        config: {
+            origin: ["https://wkm-back.vercel.app"], // URL do seu frontend na Vercel
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            allowedHeaders: [
+                "Content-Type",
+                "Authorization",
+                "Origin",
+                "X-Requested-With",
+            ],
+        },
+    },
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
